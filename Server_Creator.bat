bf37ee3fe3f7616201e39abc1c8509431504bfb1
Soumajit Roy <itssoumajit.alt@gmail.com>
2023-03-21 23:15:46 +0530

    adding powershell script

--- Server_Creator.bat
+++ Server_Creator.bat
@@ -1,2 +1,6 @@
 @echo off
-ver
+
+%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -Command ". 'D:\My Files Sam\Coding\Github\One-Click-Minecraft-Server-Creator\install.ps1' "
+
+pause
+exit /b
