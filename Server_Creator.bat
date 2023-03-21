6f99ba8d9d92541be04069095c6798aa4ded3aff
Soumajit Roy <itssoumajit.alt@gmail.com>
2023-03-22 01:19:00 +0530

    changing script powershell to python

--- Server_Creator.bat
+++ Server_Creator.bat
@@ -1,6 +1,6 @@
 @echo off
 
-%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -Command ". 'D:\My Files Sam\Coding\Github\One-Click-Minecraft-Server-Creator\install.ps1' "
+python --version
 
 pause
 exit /b
